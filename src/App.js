d67e4c08e1da61606e9f33494f63c1c60edfc848
Dozz <devdozz99@gmail.com>
2023-05-08 13:47:05 +0900

    영화 api의 전반적인 정보 로드

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [rating, setRating] = useState("8.8");
+  const [movies, setMovies] = useState([]);
+  const getMovies = async (rate) => {
+    //영화 디테일 url
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=${rate}&sort_by=year`
+      )
+    ).json();
+
+    console.log(json.data.movies);
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (loading) {
+      getMovies(rating);
+    }
+  }, [loading, rating]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((mv) => (
+            <div key={mv.id}>
+              <img alt={mv.title} src={mv.medium_cover_image} />
+              <h2>{mv.title}</h2>
+              <p>{mv.summary}</p>
+              <ul>
+                {mv.genres.map((gr, index) => (
+                  <li key={index}>{gr}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
